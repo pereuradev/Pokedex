8b06ad2e677912e49f268258c8d7f1a856ef20c5
Pietro Strummiello Pereira <pereirapietro636@gmail.com>
2026-09-15 17:54:24 -0300

    implementa o consumo da API funcional, apenas para pesquisa de dados

--- Js/Backend.js
+++ Js/Backend.js
@@ -1,14 +1,27 @@
 const irParaPokemonPesquisa = () => {
   window.location.href = "/pages/pokemonPesquisa.html";
 };
-const ConsumirApi = async (name) => {
+async function ConsumirApi(nomePokemon) {
+  const entradaNome = document.getElementById("nome");
+  const botao = document.getElementById("botao-pesquisar");
+  nomePokemon = entradaNome.value.trim();
   try {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-    const data = await response.json();
-    const input = document.getElementById("pokemon-id");
-    input.value = data.id;
-    return data;
+    botao.disabled = true;
+    const resposta = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`,
+    );
+    if (!resposta.ok) {
+      throw new Error("Pokemon não encontrado!");
+    }
+    if (resposta.ok) {
+      botao.disabled = false;
+      const dados = await resposta.json();
+      const id = document.getElementById("pokemon-id");
+      id.value = dados.id || "";
+      const altura = document.getElementById("pokemon-height");
+      altura.value = dados.height || "";
+    }
   } catch (error) {
     console.error("Erro ao consumir a API:", error);
   }
-};
+}
